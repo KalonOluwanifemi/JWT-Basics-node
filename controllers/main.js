d50047c431c26be7d11e0dabb5e9f00aae7bc779
kalon <akandeoluwanifemi@gmail.com>
2024-05-25 14:34:06 +0100

    handled when username and password were not provided with custom error

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,12 @@
+const CustomAPIError = require("../errors/custom-error");
 const login = async (req, res) => {
   const username = req.body["username"];
   const password = req.body["password"];
+
+  if (!username || password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+
   console.log(username, password);
   res.send("Fake LogIn");
 };
