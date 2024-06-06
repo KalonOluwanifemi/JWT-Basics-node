28b12d797d367d3b98d1d0c8c25a42734001d253
kalon <akandeoluwanifemi@gmail.com>
2024-06-06 15:20:06 +0100

    I aaded the authentication middleware

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,4 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
-
 const login = async (req, res) => {
   const { username, password } = req.body;
 
@@ -18,24 +16,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new CustomAPIError("No token present", 401);
-  }
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("not authorized to access this route", 401);
-  }
-
-  console.log(token);
+  console.log(req.user);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
